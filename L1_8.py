de3d7777ab2bfab691f7c60f322e46af9ce6d1bb
shamelashahnas <shamelashahnas@gmail.com>
2023-10-20 09:07:11 +0530

    added l18

---
+++ L1_8.py
@@ -0,0 +1,23 @@
+a=[22,22,33,45,65,22,77,33,65,44,33,33,33]
+i=1
+while i<len(a):
+    if a[i] in a[0:i]:
+        a.pop(i)
+    else:
+        i+=1
+print(a)
+
+
+
+
+
+# b=[]
+# for x in a:
+#
+#     if x not in b:
+#         b.append(x)
+#
+# print(b)
+
+
+
